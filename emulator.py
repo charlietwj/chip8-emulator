b1d721c7651361ccb1200bfbde0d098e131d4e32
Charlie Jones <jones@Mac.lan>
2025-11-27 23:40:45 -0800

    create constructor for chip8

--- emulator.py
+++ emulator.py
@@ -1,7 +1,16 @@
 import pygame
 
 class Chip8:
-    pass
+    
+    def __init__(self):
+        self.memory = [0] * 4096
+        self.display = [[0] * 64 for _ in range(32)]
+        self.PC = 0x200
+        self.I = 0
+        self.stack = []
+        self.delay_timer = 0
+        self.sound_timer = 0
+        self.registers = [0] * 16
 
 def read_chip8(name: str) -> None:
     lines = []
